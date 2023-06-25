9402a6629d1a62a19d1cd3db2ed9ff621c4b6de6
AinoaIglesias <72820137+alu0101164403@users.noreply.github.com>
2023-06-25 19:44:31 +0100

    Update http-commons.tsx

--- frontend-web/http-commons.tsx
+++ frontend-web/http-commons.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://127.0.0.1:8080', // direccion de 
+  //baseURL: 'http://127.0.0.1:8080', // direccion de 
+  baseURL: 'https://tfg-beckend.vercel.app/',
   headers: {
     'Content-type': 'application/json',
   },
