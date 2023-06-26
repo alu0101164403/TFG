ec083c1c8acfea2e1ee592499af4c82ae8ef261e
alu0101164403 <alu0101164403@ull.edu.es>
2023-06-26 17:12:47 +0100

    cambio link backend en railway

--- frontend-web/http-commons.tsx
+++ frontend-web/http-commons.tsx
@@ -4,7 +4,7 @@ const apiUrl = process.env.API_URL;
 
 export default axios.create({
   //baseURL: 'http://127.0.0.1:8080',
-  baseURL: apiUrl,
+  baseURL: 'https://tfg-production-0e48.up.railway.app',
   headers: {
     'Content-type': 'application/json',
   },
