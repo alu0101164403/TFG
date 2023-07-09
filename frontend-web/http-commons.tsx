a614ac521aa4a9d47a9a5e8c2d7ad62687399742
AinoaIglesias <72820137+alu0101164403@users.noreply.github.com>
2023-07-09 10:35:39 +0100

    Update http-commons.tsx

--- frontend-web/http-commons.tsx
+++ frontend-web/http-commons.tsx
@@ -4,7 +4,8 @@ const apiUrl = process.env.API_URL;
 
 export default axios.create({
   //baseURL: 'http://127.0.0.1:8080',
-  baseURL: 'https://tfg-production-0e48.up.railway.app',
+  //baseURL: 'https://tfg-production-0e48.up.railway.app',
+  baseURL: 'https://tfg-backend-ainoa.onrender.com/',
   headers: {
     'Content-type': 'application/json',
   },
